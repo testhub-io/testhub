f563ce60753a6872783b5ebca34cf93eac361226
Denis Palnitsky <palnitsky@gmail.com>
2019-11-25 00:00:11 +0100

    Revert "Don't duplicate requestst to API"
    This reverts commit 4f6ad494adf42d4bf7cc8ee5ad372c87ab9439e9.

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -3,7 +3,6 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { retry, catchError } from 'rxjs/operators';
 import { throwError, Observable } from 'rxjs';
 import { environment } from './../../environments/environment';
-import { pluck, share, shareReplay, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -18,10 +17,7 @@ export class ApiService {
     return this.http.get<any>(this.apiURL + uri)
     .pipe(
       retry(1),
-      tap(_ => console.log('executed')),
-      pluck('url'),
-      catchError(this.errorHandl),
-      shareReplay(1),
+      catchError(this.errorHandl)
     );
   }
 
