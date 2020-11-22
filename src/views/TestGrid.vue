878f468c9ee52e67366f0c77a099766dacc98ac1
Felix Nyandieka <nyandiekafelix@gmail.com>
2020-11-22 12:56:58 +0300

    fix search & add a loader component

--- src/views/TestGrid.vue
+++ src/views/TestGrid.vue
@@ -12,7 +12,6 @@
       <!-- Overview Tab -->
       <b-tab title="Overview" @click.prevent="gotoProjectRuns"></b-tab>
 
-
       <!-- Test Grid Tab -->
       <b-tab title="Test Grid" active>
         <div class="filter-block__search-block mt-3">
@@ -20,9 +19,8 @@
           <button type="submit" title="Search" class="search-button"><i class="icon-search"></i></button>
         </div>
 
-        <div class="testrun-block__table-wrapper mt-3" v-if="filteredTestResults.length">
-          
-          
+        <div class="testrun-block__table-wrapper mt-3" v-if="mappedTests.length">
+           
           <div v-for="group in filteredTestResults" :key="group.className" class="groupedClass">
           
             <div class="testrun-block__table-title" v-if="group.test.length">{{ group.className }}</div>
@@ -31,17 +29,17 @@
 
               <div v-for="(test, index) in group.test" :key="index" class="testrun-block__table-item">
                 <div class="testrun-block__table-row">
-                  <div class="testrun-block__table-main-td selectable"> {{ test.name }} </div>
+                  <div class="testrun-block__table-main-td"> {{ test.name }} </div>
                   
                   <div class="testrun-block__table-results-td">
                     <div class="dashboard-block__results-cells">
                       <TestHistoryCell 
-                        v-for="(run, runIndex) in Object.keys(test.testRuns)"
+                        v-for="run in Object.keys(test.testRuns)"
                         :testResult="test.testRuns[run]"
                         :ref="run"
                         @mouseover.native="highlightColumn(run)"
                         @mouseleave.native="removeHighlight(run)"
-                        :key="runIndex">
+                        :key="test.id + run">
                       </TestHistoryCell>
                     </div>
                   </div>
@@ -51,15 +49,18 @@
             </div>
           </div>
         </div>
+
+        <Loader v-else></Loader>
       </b-tab>
     </b-tabs>
   </div>
 </template>
 <script>
   import TestHistoryCell from '../components/TestHistoryCell'
+  import Loader from '../components/Loader.vue'
 
   export default {
-    components: { TestHistoryCell },
+    components: { TestHistoryCell, Loader },
     data() {
       return {
         searchString: null,
@@ -71,25 +72,19 @@
     },
     computed: { },
     watch: {
-      searchString() {  
+      searchString() {     
         if (this.searchString) {
-          const filter = test => test.includes(this.searchString)
+          const filter = test => test.name.includes(this.searchString)
+          const tests = [...this.testClasses]
 
-          const groups = this.mappedTests.filter(group =>  
-            Object.keys(group.testCases).some(filter))
+          const groups = tests.filter(group => group.test.some(filter))
 
-          const filtered = groups.map(grp => {
-            const matches = Object.keys(grp.testCases).filter(filter)
-
-            const filteredCases = {}
-            matches.forEach(key => { filteredCases[key] = grp.testCases[key] })
-
-            grp.testCases = filteredCases
-            return grp
-          })
-          this.filteredTestResults = filtered
+          this.filteredTestResults = groups.map(grp => {
+              const matches = grp.test.filter(filter)
+              return { ...grp, test: matches }
+            })
         } else {              
-          this.filteredTestResults = this.mapTestsToClasses(this.testClasses, this.testRuns)
+          this.filteredTestResults = this.mappedTests
         }
       }
     },
@@ -111,7 +106,6 @@
               })
             })
           });
-
 
           return group;
         });
@@ -135,17 +129,18 @@
         })
       },
       
-      async fetchTests() {       
+      fetchTests() {       
         const url = `${this.$route.params.org}/projects/${this.$route.params.project}/tests?runsLimit=42`
         
-        const testsData = await this.$http.get(url)                 
-
-        const { data, tests } = testsData.data
+        this.$http.get(url) 
+          .then(response => {
+            const { data, tests } = response.data
         
-        this.testRuns = data
-        this.testClasses = tests
+            this.testRuns = data
+            this.testClasses = tests
 
-        this.mappedTests = this.filteredTestResults = this.mapTestsToClasses(tests, data)
+            this.mappedTests = this.filteredTestResults = this.mapTestsToClasses(tests, data)
+          })                
       }
     },
     mounted() {
@@ -161,20 +156,8 @@
   .testrun-block__table-main-td {
     width: 400px;
   }
- 
-  .testrun-block__table-row {
-    pointer-events: none
-  }
-
-  .testrun-block__table-results-td {
-    pointer-events: auto
-  }
-
+  
   .highlighted {
     background-color: rgba(0,56,255, 0.5) !important;
-  } 
-
-  .selectable {
-    user-select: auto !important;
   }
 </style>
