df2e8a6c7d10802ffbc20f29e74221fc0a4735ce
Denis Palnitsky <palnitsky@gmail.com>
2021-02-06 09:46:23 +0100

    Add Travis and Circle CIs to API token auto retreival

--- pkg/api_token.go
+++ pkg/api_token.go
@@ -7,7 +7,6 @@ import (
 	"net/http"
 	"os"
 	"strings"
-	"testhub-cli/pkg/console"
 )
 
 // We have to use it only when we take org name from environment and not from user
@@ -19,14 +18,14 @@ func getApiKey(orgAndProject string, testhubDomain string, onPremise bool) (stri
 		return "", err
 	}
 
-	if !isGithubActionEnv(orgAndProject) && !isOnPremiseEnv(onPremise, testhubDomain) {
-		return "", fmt.Errorf("Api Token must be provided as an argument")
+	if isGithubActionEnv(orgAndProject) || isTravisCiEnv(orgAndProject) || isCircleCiEnv(orgAndProject) || isOnPremiseEnv(onPremise, testhubDomain) {
+		log.Println("Auto-retrieval of API token")
 	} else {
-		console.PrintLn("Auto-retrieval of API token")
+		return "", fmt.Errorf("Api Token must be provided as an argument")
 	}
 
 	url := fmt.Sprintf("%s/api/%s/apikey", testhubDomain, org)
-	log.Printf("Requestin token url: %s", url)
+	log.Printf("Requesting token url: %s", url)
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 	req.Header.Set("token", "WinLost2020$")
 	if err != nil {
@@ -52,6 +51,35 @@ func isGithubActionEnv(orgAndProject string) bool {
 	ghActionsRepos := os.Getenv("GITHUB_REPOSITORY")
 
 	if isGhActions == "true" && strings.EqualFold(ghActionsRepos, orgAndProject) {
+		log.Println("Github actions environment detected")
+		return true
+	}
+	return false
+}
+
+func isTravisCiEnv(orgAndProject string) bool {
+	isTravis := os.Getenv("TRAVIS")
+	user := os.Getenv("USER")
+	home := os.Getenv("HOME")
+	repo := os.Getenv("TRAVIS_REPO_SLUG ")
+
+	if isTravis == "true" && user == "travis" && strings.Contains(home, "travis") &&
+		strings.EqualFold(repo, orgAndProject) {
+		log.Println("Travis CI environment detected")
+		return true
+	}
+	return false
+}
+
+func isCircleCiEnv(orgAndProject string) bool {
+	isTravis := os.Getenv("CIRCLECI")
+	circleJob := os.Getenv("CIRCLE_JOB")
+	work_dir := os.Getenv("CIRCLE_WORKING_DIRECTORY")
+	repoUrl := "CIRCLE_REPOSITORY_URL"
+
+	if isTravis == "true" && len(circleJob) != 0 && len(work_dir) != 0 &&
+		strings.Contains(strings.ToLower(orgAndProject), strings.ToLower(repoUrl)) {
+		log.Println("Circle CI environment detected")
 		return true
 	}
 	return false
