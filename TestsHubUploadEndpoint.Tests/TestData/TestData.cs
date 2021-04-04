e5f3f75e20b02613a0a0293591db5a9e8d61cf11
Denis Palnitsky <palnitsky@gmail.com>
2021-04-04 15:13:22 +0200

    rearrange test files

--- TestsHubUploadEndpoint.Tests/TestData/TestData.cs
+++ TestsHubUploadEndpoint.Tests/TestData/TestData.cs
@@ -5,6 +5,16 @@ namespace TestsHubUploadEndpoint.Tests
 {
     internal static class TestData
     {
+        public static Stream GetTestReport(string type, string name)
+        {
+            return GetFile(Path.Join("test-reports", type, name));
+        }
+
+        public static Stream GetCoverageReport(string type, string name)
+        {
+            return GetFile(Path.Join("coverage", type, name));
+        }
+
         public static Stream GetFile(string name)
         {
             if (!File.Exists($"../../../TestData/{name}"))
